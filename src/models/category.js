c5b803650693d2a473a3fc1fe6da7a7a3b397de2
yohanescms <yohanes.2710cms@gmail.com>
2025-01-05 23:00:50 +0700

    add image

--- src/models/category.js
+++ src/models/category.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const categorySchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
+  imageUrl: { type: String }, // URL gambar kategori
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 });
